e7a69573d3934e3c21622c2e75d326065a91f252
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-21 16:41:19 +0100

    Added header markup

--- header.php
+++ header.php
@@ -10,36 +10,38 @@
   <body>
 
     <header role="banner" class="header">
-      <h1 class="header__title">
-        <a rel="home" href="<?php echo esc_url(home_url('/')); ?>">
-          <span class="hidden"><?php bloginfo('name'); ?></span>
-          <img src="<?php bloginfo('template_url'); ?>/dist/img/logo.png" alt="">
-        </a>
-      </h1>
       <div class="container">
 
-      <?php if (has_nav_menu('menu')): ?>
-        <div class="header__toggle">
-          <button type="button" class="toggle" aria-label="<?php esc_html_e('Menu', 'lafrutticola'); ?>">
-            <span class="hidden"><?php esc_html_e('Menu', 'lafrutticola'); ?></span>
-            <span class="toggle__bar" aria-hidden="true"></span>
-            <span class="toggle__bar" aria-hidden="true"></span>
-            <span class="toggle__bar" aria-hidden="true"></span>
-          </button>
-        </div>
-      <?php endif; ?>
+        <h1 class="header__title">
+          <a rel="home" href="<?php echo esc_url(home_url('/')); ?>">
+            <span class="hidden"><?php bloginfo('name'); ?></span>
+            <img src="<?php bloginfo('template_url'); ?>/dist/img/logo.png" alt="">
+          </a>
+        </h1>
 
-      <?php if (has_nav_menu('menu')): ?>
-        <nav role="navigation" class="header__navigation">
-          <h2 class="hidden"><?php esc_html_e('Menu', 'lafrutticola'); ?></h2>
+        <?php if (has_nav_menu('menu')): ?>
+          <div class="header__toggle">
+            <button type="button" class="toggle" aria-label="<?php esc_html_e('Menu', 'lafrutticola'); ?>">
+              <span class="hidden"><?php esc_html_e('Menu', 'lafrutticola'); ?></span>
+              <span class="toggle__bar" aria-hidden="true"></span>
+              <span class="toggle__bar" aria-hidden="true"></span>
+              <span class="toggle__bar" aria-hidden="true"></span>
+            </button>
+          </div>
+        <?php endif; ?>
 
-          <ul>
-            <?php wp_nav_menu(array(
-              'theme_location' => 'menu',
-              'items_wrap' => '%3$s'
-            )); ?>
-          </ul>
-        </nav>
-      <?php endif; ?>
+        <?php if (has_nav_menu('menu')): ?>
+          <nav role="navigation" class="header__navigation">
+            <h2 class="hidden"><?php esc_html_e('Menu', 'lafrutticola'); ?></h2>
+
+            <ul>
+              <?php wp_nav_menu(array(
+                'theme_location' => 'menu',
+                'items_wrap' => '%3$s'
+              )); ?>
+            </ul>
+          </nav>
+        <?php endif; ?>
+
       </div>
     </header>
