5153d625e3ecf8e4819ba5a1b7747886ca1dcc03
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-22 15:42:13 +0100

    Added base markup for single page

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,5 +1,27 @@
 <article class="single">
-  <?php the_post_thumbnail(); ?>
-  <h2><?php the_title(); ?></h2>
-  <?php the_content(); ?>
+
+  <?php if (has_post_thumbnail()): ?>
+    <div class="hero">
+      <div class="hero__container">
+        <div class="hero__image">
+          <?php the_post_thumbnail('large'); ?>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <div class="heading">
+    <div class="container">
+      <h2><?php the_title(); ?></h2>
+    </div>
+  </div>
+
+  <div class="body">
+    <div class="container">
+      <div class="body__item">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+
 </article>
