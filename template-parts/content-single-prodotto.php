2810dedf1732e01a35220ae5238022763084f6a5
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-30 13:54:21 +0200

    Added single product view

---
+++ template-parts/content-single-prodotto.php
@@ -0,0 +1,38 @@
+<article class="single single--product">
+
+  <?php if (has_post_thumbnail()): ?>
+    <div class="hero">
+      <div class="hero__container">
+        <div class="hero__image">
+          <?php the_post_thumbnail('large'); ?>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <div class="heading">
+    <div class="container">
+      <h2><?php the_title(); ?></h2>
+
+      <?php
+        $product_terms = get_the_terms($post->ID, 'tipologia');
+        $product_count = count($product_terms);
+        if ($product_count > 0):
+      ?>
+        <ul>
+          <?php foreach ($product_terms as $product_term): ?>
+            <li><?php echo $product_term->name; ?></li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <div class="body">
+    <div class="container">
+      <div class="body__item">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+</article>
